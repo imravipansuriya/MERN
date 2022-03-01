c3c76ecd10ef0f7cdb31b61584bb8b29f79c6358
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-03-01 07:25:23 +0530

    while loop

--- javascript/loops/while.js
+++ javascript/loops/while.js
@@ -11,8 +11,34 @@ while( i <= 10 )// loop condition
 }
 */
 
-// WAP to print even numbers from 1 to 20 using while loop
 
+// var a = 1;
+// while (a <= 10){
+//     console.log(a);
+//     a++;
+// }
+
+// var b = 1;
+// while(b <= 50){
+//      console.log(b);
+//      b++;
+// }
+
+// var c = 11;
+// while(c <= 50){
+//     console.log(c);
+//     c++;
+// }
+
+// var d = 50;
+
+// while(d <= 100){
+//     console.log(d);
+//     d++;
+// }
+
+// WAP to print even numbers from 1 to 20 using while loop
+ 
 /*
 Method 1
 var i = 1; // loop initializer
@@ -27,18 +53,37 @@ while( i <= 20 )// loop condition
 }
 */
 
-/*
-Method 2
 
-var i = 0; // loop initializer
+var g = 0;
 
-while( i <= 20 )// loop condition
-{ 
-    // loop body
-    console.log(i);
-    i += 2; // loop updation
+while(g <= 50){
+    
+    if(g % 2 === 0){
+        console.log(g) 
+    }
+    g++;
 }
-*/
+
+
+ 
+// Method 2
+
+// var i = 0; // loop initializer
+
+// while( i <= 20 )// loop condition
+// { 
+//     // loop body
+//     console.log(i);
+//     i += 2; // loop updation
+// }
+ 
+
+// var f = 0;
+
+// while (f <= 50){
+//     console.log(f);
+//     f+= 2;
+// }
 
 // WAP to print all the digits of a number using while loop
 // e.g. if input is 569, then output is 5 6 9
