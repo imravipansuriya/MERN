3c4ff0cf9aaf73deb68bf4f501d049c02ff24b92
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-03-02 10:05:12 +0530

    frontend/ question 1-3

---
+++ javascript/loops/for.js
@@ -0,0 +1,36 @@
+// WAP print numbers from 1 to 10 using for loop
+/*
+for(var i = 1 ; i <= 10 ; i++) {
+    console.log(i)
+}
+*/
+
+// WAP to print even numbers from 1 to 20
+
+/*
+for(var i = 1 ; i <= 20 ; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+*/
+/*
+for (var i = 2; i <= 20; i += 2) {
+    console.log(i)
+}
+*/
+
+// WAP to print all the digits of a number
+
+/*
+for(var i = 569; i > 0 ; i = parseInt(i/10) ) {
+    console.log(i % 10);
+}*/
+
+// WAP to print table of a number using while loop
+/*
+var num = 5;
+for( i = 1 ; i <= 10 ; i++ ) {
+    console.log(num + " * " + i + " = " + num * i);
+}
+*/
---
+++ javascript/loops/nested-for.js
@@ -0,0 +1,33 @@
+// WAP to print numbers pattern
+/*  
+            * * * * *
+            * * * * *
+            * * * * *
+*/
+/*
+var numOfRows = 5;
+var numOfCols = 10;
+
+for (var i = 1 ; i <= numOfRows ; i++) { // for rows
+    var stars = "";
+
+    for (var j = 1 ; j <= numOfCols ; j++) { // for columns
+        stars = stars + "* "
+    }
+
+    console.log(stars);
+}
+*/
+
+/*
+// WAP to print tables from 1 to 5 using for loop loop
+
+for (var j = 1 ; j <= 5 ; j++) {
+    
+    for( i = 1 ; i <= 10 ; i++ ) {
+        console.log(j + " * " + i + " = " + j * i);
+    }
+    console.log()
+
+}
+*/
