33c764cd59935bb9c3db0e8d0d425f3633dbb829
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-03-02 11:45:51 +0530

    for loop

--- javascript/loops/for.js
+++ javascript/loops/for.js
@@ -5,6 +5,20 @@ for(var i = 1 ; i <= 10 ; i++) {
 }
 */
 
+// WAP print numbers from 1 to 20 using for loop
+
+
+// for (var i = 1; i <= 20; i++){
+//     console.log(i)
+// }
+
+// WAP print numbers from 5 to 50 using for loop
+
+// for (var i = 5; i <= 50; i++){
+//     console.log(i)
+// }
+
+
 // WAP to print even numbers from 1 to 20
 
 /*
@@ -14,18 +28,36 @@ for(var i = 1 ; i <= 20 ; i++) {
     }
 }
 */
+
+// WAP to print even numbers from 1 to 50
+
+// for(var i = 1; i <= 50; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
 /*
 for (var i = 2; i <= 20; i += 2) {
     console.log(i)
 }
 */
 
+// for (let i = 1; i <= 50; i += 2) {
+//     console.log(i)
+// }
+
 // WAP to print all the digits of a number
+
 
 /*
 for(var i = 569; i > 0 ; i = parseInt(i/10) ) {
     console.log(i % 10);
 }*/
+
+// for (let i = 12345; i > 0; i = parseInt(i/10)) {
+//     console.log(i % 10)
+// }
 
 // WAP to print table of a number using while loop
 /*
@@ -34,3 +66,8 @@ for( i = 1 ; i <= 10 ; i++ ) {
     console.log(num + " * " + i + " = " + num * i);
 }
 */
+
+var num1 = 10;
+for (var i = 1; i <= num1; i++){
+    console.log(num1 + " * " + i + " = " + i * num1 )
+}
---
+++ practice/questions-23-02.js
@@ -0,0 +1,26 @@
+// 1. declare 2 variables, and print their sum.
+
+var car1 = 10;
+var car2 = 20;
+
+console.log(car1 + car2)
+
+// 2. Declare 3 variables a,b and c; print sum of a and b, remainder when b is divided by c.
+
+var a = 10;
+var b = 20;
+var c = 30;
+
+console.log(a + b)
+console.log(b / c)
+
+
+// 3. declare 2 variables and print the vaiable with larger value
+
+var product1 = 10
+var product2 = 20
+
+product1 < product2 ? console.log(product1) : console.log(product2)
+
+
+    
