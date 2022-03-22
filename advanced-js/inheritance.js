19e5adac14207ccf3e1c41a89f3a55579b0ba02f
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-03-22 21:59:12 +0530

    inharitans

--- advanced-js/inheritance.js
+++ advanced-js/inheritance.js
@@ -9,6 +9,10 @@ Class Inheritance
 
 To create a class inheritance, use the extends keyword.
 
+
+The super() method refers to the parent class. By calling the super() method in 
+the constructor method, we call the parent's constructor method and gets access to the parent's properties and methods.
+
 */
 
 class Animal {
