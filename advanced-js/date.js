b9154de43279ac40c6d45f47b5f4d0d617ea9b62
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-03-27 20:44:46 +0530

    front end changes

---
+++ advanced-js/date.js
@@ -0,0 +1,25 @@
+// IST: India Standard Time +5:30 
+// GMT: Greenwich Mean Time +0:00
+// EST: Eastern Standard Time -5:00
+
+const date = new Date();
+// console.log(date)
+
+// console.log(date.toISOString())
+// console.log(date.toUTCString())
+// console.log(date.toDateString())
+// console.log(date.toTimeString())
+// console.log(date.toLocaleString())
+// console.log(date.toLocaleDateString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.getTime()) // gives the time in milliseconds since 1 Jan 1970
+// console.log(date.getFullYear())
+// console.log(date.getMonth()) // gives the month in number starting from 0
+// console.log(date.getDate())
+// console.log(date.getDay()) // gives the day in number starting from 0, 0 is sunday, 6 is saturday
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
+console.log(date.getTimezoneOffset())
+
