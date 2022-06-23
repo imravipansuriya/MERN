da1cc7b5532337cd8111d072b149c7bcc826b7d0
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-06-24 01:39:31 +0530

    node

---
+++ flights/mongo/db.js
@@ -0,0 +1,22 @@
+const {MongoClient} = require('mongodb') // require mongodb
+const url = 'mongodb+srv://ravi:ravi@cluster0.p6rse.mongodb.net/replit?retryWrites=true&w=majority'; // mongodb url
+const database = 'replit' // database name
+const client = new MongoClient(url)  // set data url in mondodb
+
+async function getData(){
+    // let result = await client.connect() // connect with database
+    // let db = result.db(database) // databash name
+    // let collection = db.collection('flights')
+    // let response = await collection.find({}).toArray();
+    // console.log(response)
+
+    MongoClient.connect(url, {}).then(()=>{
+        console.log('* mondodb is connected *')
+    }).catch( err => {
+        console.log(err)
+    })
+
+    
+}
+
+module.exports = getData;
